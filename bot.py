71a9073130120c4db86748a1df15d8ef268e78ad
abdullaaliev <78908123+abdullaaliev@users.noreply.github.com>
2026-04-12 13:58:11 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,118 @@
+import asyncio
+import os
+import json
+from datetime import datetime, time
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+API_TOKEN = os.getenv("API_TOKEN")
+
+# --- Google Sheets ---
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
+
+creds_dict = json.loads(os.getenv("GOOGLE_CREDS"))
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    creds_dict, scope
+)
+
+client = gspread.authorize(creds)
+sheet = client.open("Отчет").sheet1
+
+# --- Bot ---
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+user_daily_log = {}
+users = set()
+
+
+@dp.message(F.text == "/start")
+async def start(message: Message):
+    users.add(message.from_user.id)
+
+    await message.answer(
+        "👋 Привет!\n\n"
+        "✂️ Напиши, сколько изделий ты изготовила сегодня.\n\n"
+        "Пример: 25"
+    )
+
+
+@dp.message(F.text)
+async def save_data(message: Message):
+    if not message.text.isdigit():
+        await message.answer("❌ Пиши только число")
+        return
+
+    user_id = message.from_user.id
+    users.add(user_id)
+
+    count = int(message.text)
+    today = datetime.now().strftime("%d.%m.%Y")
+
+    if user_id in user_daily_log and user_daily_log[user_id]["date"] == today:
+        await message.answer("⚠️ Уже отправляла сегодня")
+        return
+
+    name = message.from_user.full_name
+    username = message.from_user.username or ""
+
+    user_daily_log[user_id] = {
+        "date": today,
+        "count": count
+    }
+
+    salary = count * 70
+
+    await message.answer(
+        f"✅ Принято: {count}\n💰 ЗП: {salary} ₽"
+    )
+
+    asyncio.create_task(write_to_sheet(today, name, username, count, salary))
+
+
+async def write_to_sheet(date, name, username, count, salary):
+    await asyncio.to_thread(
+        sheet.append_row,
+        [date, name, username, count, salary]
+    )
+
+
+async def wait_until(target_time: time):
+    while True:
+        now = datetime.now()
+        target = datetime.combine(now.date(), target_time)
+
+        if now >= target:
+            target = target.replace(day=now.day + 1)
+
+        await asyncio.sleep((target - now).total_seconds())
+        return
+
+
+async def reminder_loop():
+    while True:
+        await wait_until(time(18, 0))
+
+        for user_id in users:
+            try:
+                await bot.send_message(user_id, "⏰ Сдайте отчёт за сегодня")
+            except:
+                pass
+
+
+async def main():
+    print("Бот запущен 🚀")
+    asyncio.create_task(reminder_loop())
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
